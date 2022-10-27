aa14e2850b1d6e070afd7210cc471340e3c3e08c
Yifan Ye <yifanye@Yifans-MacBook-Pro.local>
2022-10-27 18:40:16 +0200

    Add user authentication

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { JwtModule } from '@nestjs/jwt'
+import { secret } from './utils/constants'
+import { join } from 'path/posix'
 
 @Module({
     imports: [
         MongooseModule.forRoot(
             'mongodb+srv://yifan:root@cluster0.8pojb.mongodb.net/?retryWrites=true&w=majority'
-        )
+        ),
+        JwtModule.register({
+            secret,
+            signOptions: { expiresIn: '2h' }
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'public')
+        })
     ],
     controllers: [AppController],
     providers: [AppService]
