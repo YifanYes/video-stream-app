3bf3f756450f6f921ced07619b02579a4cc7597b
Yifan Ye <yifanye@Yifans-MacBook-Pro.local>
2022-10-27 18:21:33 +0200

    Define User and Video Schemas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot(
+            'mongodb+srv://yifan:root@cluster0.8pojb.mongodb.net/?retryWrites=true&w=majority'
+        )
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
---
+++ backend/src/model/user.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+export type UserDocument = User & Document
+
+@Schema()
+export class User {
+    @Prop({ required: true })
+    fullname: string
+    @Prop({ required: true, unique: true, lowecase: true })
+    email: string
+    @Prop({ required: true })
+    password: string
+    @Prop({ default: Date.now() })
+    createdDate: Date
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
