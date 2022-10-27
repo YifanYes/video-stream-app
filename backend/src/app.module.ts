c56ff748a2fb9a0524fe6deb6522d56785f9b284
Yifan Ye <yifanye@Yifans-MacBook-Pro.local>
2022-10-27 18:45:56 +0200

    Add video controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,12 +6,24 @@ import { ServeStaticModule } from '@nestjs/serve-static'
 import { JwtModule } from '@nestjs/jwt'
 import { secret } from './utils/constants'
 import { join } from 'path/posix'
+import { MulterModule } from '@nestjs/platform-express'
+import { diskStorage } from 'multer'
+import { v4 as uuidv4 } from 'uuid'
 
 @Module({
     imports: [
         MongooseModule.forRoot(
             'mongodb+srv://yifan:root@cluster0.8pojb.mongodb.net/?retryWrites=true&w=majority'
         ),
+        MulterModule.register({
+            storage: diskStorage({
+                destination: './public',
+                filename: (req, file, cb) => {
+                    const ext = file.mimetype.split('/')[1]
+                    cb(null, `${uuidv4()}-${Date.now()}.${ext}`)
+                }
+            })
+        }),
         JwtModule.register({
             secret,
             signOptions: { expiresIn: '2h' }
